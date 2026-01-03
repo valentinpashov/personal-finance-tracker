f990996291d171ded6af644abc1bbce075f135ed
Valentin Pashov <valentinpashov04@gmail.com>
2026-01-03 16:35:31 +0200

    Updated project's structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import Register from "./componets/Register";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div>
-      <h1>Personal Finance Tracker</h1>
-      <Register />
-    </div>
+    <Router>
+      <div style={{ padding: "20px" }}>
+        <h1>Personal Finance Tracker</h1>
+        
+        <Routes>
+          <Route path="/" element={<Navigate to="/register" />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        
+      </div>
+    </Router>
   );
 }
 
