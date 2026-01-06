fbb9cce78d2681ba7e1073b5e8495f4b8f731bd7
Valentin Pashov <valentinpashov04@gmail.com>
2026-01-06 18:49:23 +0200

    Add categories

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -10,7 +10,24 @@ const Dashboard = () => {
         <h1>Твоето финансово табло</h1>
         <p>Тук скоро ще виждаш графика на разходите си.</p>
         
-        
+        <div className="stats-grid">
+           
+           <div className="card">
+              <h3>Приходи</h3>
+              <p className="green">0.00 лв.</p>
+           </div>
+
+           <div className="card">
+              <h3>Разходи</h3>
+              <p className="red">0.00 лв.</p>
+           </div>
+           
+           <div className="card">
+              <h3>Баланс</h3>
+              <p className="blue">0.00 лв.</p>
+           </div>
+
+        </div>
       </div>
     </div>
   );
