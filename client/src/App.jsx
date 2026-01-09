309b39ea67ef935a1cdddbfa3ccbe05f2f2bb6ed
Valentin Pashov <valentinpashov04@gmail.com>
2026-01-09 18:30:17 +0200

    Updated structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
+import CalendarPage from "./components/CalendarPage";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+
+          <Route path="/calendar" element={<CalendarPage />} />
         </Routes>
     </Router>
   );
