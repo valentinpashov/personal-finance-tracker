9a23db49c061c636063fd956a682f3813142aa56
Valentin Pashov <valentinpashov04@gmail.com>
2026-01-15 17:51:27 +0200

    Add Footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="site-footer">
+      <div className="footer-content">
+        
+        
+
+      </div>
+
+      <div className="footer-bottom">
+        <p>&copy; 2026 Finance Tracker. Всички права запазени.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
