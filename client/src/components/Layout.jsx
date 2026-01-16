6063b302b04152555c2dc661e189eac2f3008354
Valentin Pashov <valentinpashov04@gmail.com>
2026-01-16 15:09:40 +0200

    Add layout

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer"; 
+
+const Layout = ({ setAuth }) => {
+  return (
+    <div className="page-wrapper" style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      
+      <Navbar setAuth={setAuth} />
+
+      <div style={{ flex: 1, width: "100%" }}>
+        <Outlet />
+      </div>
+
+      <Footer />
+      
+    </div>
+  );
+};
+
+export default Layout;
